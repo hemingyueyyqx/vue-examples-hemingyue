73b512204f0a22fbfe96a0e815815a66b2b7f482
何明月 <2087144700@qq.com>
2025-03-26 14:42:02 +0800

    defineProps defineEmits

--- src/views/example08/Example08-1.vue
+++ src/views/example08/Example08-1.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
 import Child from './Example08-2.vue'
+import { ref } from 'vue'
+// 定义响应式变量
+let car = ref('bmw')
+let toy = ref('') //需要子传递的变量
+let toy1 = ref('') //需要子传递的变量
+//传递方法
+function getToy(value: string) {
+  console.log('父', value)
+  toy.value = value
+}
+//测试$event(特殊占位符，事件对象)
+const test = (x: number, y: Event) => {
+  console.log('test', x, y)
+}
 </script>
 <template>
   <div>
     <div id="parent">
       <h3>父组件</h3>
-      <Child />
+      <h4>汽车：{{ car }}</h4>
+      <h4>
+        子给的玩具：{{ toy }}（父定义方法，方法传递给子，子调用props方法，将数据作为参数传递给父）
+      </h4>
+      <h4>子给的玩具1：{{ toy1 }}</h4>
+      <button @click="test(6, $event)">测试$event</button>
+      <!-- 给子组件绑定事件 get-toy自定义事件名 子中要声明 -->
+      <Child :car="car" :sendToy="getToy" @get-toy="toy1 = $event" />
     </div>
   </div>
 </template>
--- src/views/example08/Example08-2.vue
+++ src/views/example08/Example08-2.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+// 定义响应式变量
+let toy = ref('wawa')
+let toy1 = ref('奥特曼')
+// 声明接受 props
+const props = defineProps(['car', 'sendToy'])
+// 定义要触发的事件
+const emit = defineEmits(['get-toy'])
+// 触发事件的函数
+const sendToyToParent = () => {
+  console.log(emit)
+  emit('get-toy', toy1.value)
+}
+</script>
+
 <template>
-  <div id="child">子组件</div>
+  <div id="child">
+    <h3>子组件</h3>
+    <h4>玩具：{{ toy }}</h4>
+    <h4>玩具1：{{ toy1 }}</h4>
+    <h4>父给的车：{{ car }} (defineProps直接传入数据)</h4>
+    <button @click="sendToy(toy)">把玩具给父亲</button>
+    <!-- <button @click="emit('get-toy', toy1)">把玩具1给父亲</button> -->
+    <button @click="sendToyToParent">把玩具1给父亲</button>
+  </div>
 </template>
+
 <style scoped>
 #child {
   border: 5px solid red;
